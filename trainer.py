7b02c07270cbcc19eaa23e29fc38f763f37a170e
JohnnyY8 <johnny9141@hotmail.com>
2018-10-07 23:43:16 -0700

    Initial version.

--- trainer.py
+++ trainer.py
@@ -15,7 +15,6 @@ class Trainer:
   def train_dnn(self):
     self.x_train, self.x_test, self.y_train, self.y_test = \
         self.ins_dataprocess.split_data_2train_and_test()
-    #print(self.x_train.shape, self.x_test.shape, self.y_train.shape, self.y_test.shape)
 
     with tf.Session() as sess:
       train_accu_old, train_accu_new, test_accu_best = 0.0, 0.0, 0.0
@@ -27,22 +26,22 @@ class Trainer:
       while True:
         train_index = np.array(range(self.x_train.shape[0]))
         random.shuffle(train_index)
-        print("No.%d epoch is starting..." % (num_epoches))
-        for ind in xrange(0,
-            self.x_train.shape[0],
-            self.FLAGS.batch_size):
+
+        print("No.%d epoch:" % (num_epoches)),
+        for ind in xrange(0, self.x_train.shape[0], self.FLAGS.batch_size):
 
           batch_xs, batch_ys = \
               self.x_train[train_index[ind: ind + self.FLAGS.batch_size]], \
               self.y_train[train_index[ind: ind + self.FLAGS.batch_size]]
 
           train_loss, temp = sess.run(
-                [self.ins_model.loss,
-                 self.ins_model.train_step],
-                feed_dict = {
-                    self.ins_model.x_data: batch_xs,
-                    self.ins_model.y_label: batch_ys,
-                    self.ins_model.keep_prob: self.FLAGS.dropout_rate})
+              [self.ins_model.loss,
+               self.ins_model.train_step],
+              feed_dict = {
+                  self.ins_model.x_data: batch_xs,
+                  self.ins_model.y_label: batch_ys,
+                  self.ins_model.keep_prob: self.FLAGS.dropout_rate})
+
         print(train_loss)
 
         #if test_accu_new > test_accu_best:
