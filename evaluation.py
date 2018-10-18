bbcb534588cfdb48ae59925486cb4b024c284b7c
JohnnyY8 <johnny9141@hotmail.com>
2018-10-18 15:52:17 -0700

    Modify for evaluation functions.

--- evaluation.py
+++ evaluation.py
@@ -8,32 +8,25 @@ class Evaluation:
     self.num_atoms = ins_dataprocess.num_atoms
 
   def get_all_evaluation(self, true_values, predicted_values):
-    true_energy, predicted_energy = \
-        true_values[:, 0], \
-        predicted_values[:, 0]
-    #print true_energy.shape, predicted_energy.shape
-    self.evaluate_energy(true_energy, predicted_energy)
-
-    true_force, predicted_force = \
-        true_values[:, 1: self.num_atoms * 3 + 1], \
-        predicted_values[:, 1: self.num_atoms * 3 + 1]
+    self.evaluate_force(true_values, predicted_values)
+    #true_force, predicted_force = \
+    #    true_values[:, 1: self.num_atoms * 3 + 1], \
+    #    predicted_values[:, 1: self.num_atoms * 3 + 1]
     #print true_force.shape, predicted_force.shape
-    self.evaluate_force(true_force, predicted_force)
+    #self.evaluate_force(true_force, predicted_force)
 
-    true_velocity, predicted_velocity = \
-        true_values[:, self.num_atoms * 3 + 1: ], \
-        predicted_values[:, self.num_atoms * 3 + 1: ]
+    #true_energy, predicted_energy = \
+    #    true_values[:, 0], \
+    #    predicted_values[:, 0]
+    #self.evaluate_energy(true_energy, predicted_energy)
+
+    #true_velocity, predicted_velocity = \
+    #    true_values[:, self.num_atoms * 3 + 1: ], \
+    #    predicted_values[:, self.num_atoms * 3 + 1: ]
     #print true_velocity.shape, predicted_velocity.shape
-    self.evaluate_velocity(true_velocity, predicted_velocity)
-
-  def evaluate_energy(self, true_energy, predicted_energy):
-    mean_error_energy = np.mean(np.abs(np.subtract(true_energy, predicted_energy)))
-    print("  The mean error of energy is: " + str(mean_error_energy))
+    #self.evaluate_velocity(true_velocity, predicted_velocity)
 
   def evaluate_force(self, true_force, predicted_force):
-    #print(true_force[: 2])
-    #print(predicted_force[: 2])
-    #raw_input("...")
     mean_error_force = np.mean(np.abs(np.subtract(true_force, predicted_force)))
     print("  The mean error of force is: " + str(mean_error_force))
 
@@ -46,10 +39,11 @@ class Evaluation:
     print("  The mean error of force in each direction is: " + \
         str(mean_error_force_each_direction))
 
+  def evaluate_energy(self, true_energy, predicted_energy):
+    mean_error_energy = np.mean(np.abs(np.subtract(true_energy, predicted_energy)))
+    print("  The mean error of energy is: " + str(mean_error_energy))
+
   def evaluate_velocity(self, true_velocity, predicted_velocity):
-    #print(true_velocity[: 2])
-    #print(predicted_velocity[: 2])
-    #raw_input("...")
     mean_error_velocity = np.mean(np.abs(np.subtract(true_velocity, predicted_velocity)))
     print("  The mean error of velocity is: " + str(mean_error_velocity))
 
